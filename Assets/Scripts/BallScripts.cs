b90678e08dab8671b0ba22cd634b5b2a601fddcb
NellyTCu <NellyTCu@gmail.com>
2017-06-12 13:08:39 +0700

    Loi voi BallScript

--- Assets/Scripts/BallScripts.cs
+++ Assets/Scripts/BallScripts.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BallScripts : MonoBehaviour {
-
+	public int  Number_Ball=10;
+	public GameObject ball;
+	public GameObject[] balls;
+	public float moveSpeed = 10f;
 	// Use this for initialization
+	public float spawnWait=2f;
 	void Start () {
 		
+		StartCoroutine (Spawn());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
 	}
 	void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.tag == "Square") {
 			Destroy (gameObject);
+		}
+	}
+	IEnumerator Spawn(){
+		for (int i = 0; i <= Number_Ball; i++){
+			GameObject ball= GameObject.FindWithTag("Ball");
+			GameObject Ball = Instantiate (ball);
+			Ball.SetActive (false);
+			balls[i]=Ball;
+			yield return new WaitForSeconds (spawnWait);
+			Debug.Log (i);
 		}
 	}
 }
